8e4472cbb244c4497a0b0101ee2e05fc7c0b39cf
FToovvr <FToovvr@protonmail.com>
2020-08-20 12:39:45 +0800

    支持更灵活的命令行参数

--- thread-renderer/adnmb-render-thread-dump.py
+++ thread-renderer/adnmb-render-thread-dump.py
@@ -13,6 +13,7 @@ import json
 from pathlib import Path
 from os.path import splitext
 from shutil import rmtree
+import argparse
 
 # third-patry libiraies
 import yaml
@@ -24,32 +25,53 @@ from src.generating import OutputsGenerator
 
 
 def main(args: List[str]):
-    if len(args) != 2:
-        exit_with_message(f"usage: {args[0]} <divisions.yaml>",
-                          status_code=1)
+    parser = argparse.ArgumentParser(
+        prog=args[0],
+        description="根据切割规则，从A岛串的转存文件渲染为一组markdown文件",
+    )
+    parser.add_argument("-c", "--div-config", "--divisions-configuration",
+                        help="切割规则配置文件的路径", metavar="<path to divisions.yaml>",
+                        type=Path, dest="div_cfg_path", required=True)
+    parser.add_argument("-i", "--input", '--input-dump-folder',
+                        help="输入的转存文件夹路径，默认为配置文件同目录下的`dump`文件夹", metavar="<path to dump folder>",
+                        type=Path, dest="dump_folder_path")
+    parser.add_argument("-o", "--output",
+                        help="输出文件夹路径，默认为配置文件同目录下的`book`文件夹", metavar="<path to output folder>",
+                        type=Path, dest="output_folder_path")
+    parser.add_argument("--overwrite-output",
+                        help="如果输出文件夹存在，删除并重建该文件夹",
+                        dest="overwrite_output", action="store_true", default=False)
+    args = parser.parse_args(args[1:])
+    default_base_folder_path = args.div_cfg_path.parent
+    if args.dump_folder_path == None:
+        args.dump_folder_path = default_base_folder_path / "dump"
+    if args.output_folder_path == None:
+        args.output_folder_path = default_base_folder_path / "book"
+    if args.output_folder_path.exists():
+        if args.overwrite_output:
+            rmtree(args.output_folder_path, ignore_errors=True)
+        else:
+            raise f"output folder exists: {args.output_folder_path}"
 
-    div_cfg_path = args[1]
     try:
-        with open(div_cfg_path) as div_cfg_file:
+        with open(args.div_cfg_path) as div_cfg_file:
             try:
                 div_cfg = DivisionsConfiguration.load(
                     div_cfg_file,
-                    root_folder_path=Path(div_cfg_path).parent.absolute(),
+                    root_folder_path=Path(args.div_cfg_path).parent.absolute(),
                 )
             except Exception as e:
-                exit_with_message(f"failed to load {div_cfg_path}: {e}",
+                exit_with_message(f"failed to load {args.div_cfg_path}: {e}",
                                   status_code=3)
     except Exception as e:
-        exit_with_message(f"unable to open {div_cfg_path}: {e}",
+        exit_with_message(f"unable to open {args.div_cfg_path}: {e}",
                           status_code=2)
 
-    thread = Thread.load_from_dump_folder(div_cfg.root_folder_path / "dump")
+    thread = Thread.load_from_dump_folder(args.dump_folder_path)
 
-    output_folder_path = div_cfg.root_folder_path / "book"
-    rmtree(output_folder_path, ignore_errors=True)
-    output_folder_path.mkdir(parents=True)
+    args.output_folder_path.mkdir(parents=True)
     OutputsGenerator.generate_outputs(
-        output_folder_path=output_folder_path,
+        output_folder_path=args.output_folder_path,
         thread=thread,
         configuration=div_cfg)
 
